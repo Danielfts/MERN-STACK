52a5758bfc44d0ac9590e8b49ee061d3c481e1cb
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-13 17:51:23 -0600

    completed get all books

--- backend/index.js
+++ backend/index.js
@@ -38,13 +38,27 @@ app.post('/books', async (request, response) => {
 	}
 })
 
+// Route to get all books
+app.get('/books', async (request, response)=>{
+	try {
+		const books = await Book.find({});
+		return response.status(200).json({
+			count: books.length,
+			data: books
+		});
+	} catch (error) {
+		console.log (error.message);
+		response.status(500).send({ message: error.message })
+	}
+});
+
 mongoose
 	.connect(mongoDBURL)
 	.then(()=>{
 		console.log("app connected to database");
 		app.listen(
 			PORT, ()=> {
-				console.log(`App is listening to port: ${PORT}`);
+				console.log(`App is listening to port: ${PORT} at http://localhost:${PORT}`);
 			}
 		)
 	})
