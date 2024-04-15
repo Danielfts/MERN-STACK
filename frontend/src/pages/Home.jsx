9b461dc426f8717bb028ca007ec51ebed1107360
Daniel Felipe Triviño <d.trivino@uniandes.edu.co>
2024-04-14 18:24:41 -0600

    added show type buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,7 @@ import BooksTable from '../components/home/BooksTable.jsx';
 const Home = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [showType, setShowType] = useState('table');
 
     useEffect(() => {
         setLoading(true);
@@ -29,6 +30,16 @@ const Home = () => {
     }, [])
   return (
     <div className='p-4'>
+        <div className='flex justify-center items-center gap-x-4'>
+            <button
+                className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+                onClick={()=> setShowType('table')}>Table</button>
+            <button
+                className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+                onClick={()=> setShowType('card')}>Cards</button>
+            
+        </div>
+
         <div className='flex justify-between items-center'>
             <h1 className='text-3xl my-8'>Books List</h1>
             <Link to="/books/create">
